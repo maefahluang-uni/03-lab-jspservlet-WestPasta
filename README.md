f2be8a3ad78b8d7ad2b210efcc700c5da4c45338
WestPasta <6531503154@lamduan.mfu.ac.th>
2023-09-05 15:51:38 +0700

    Finished

--- README.md
+++ README.md
@@ -29,7 +29,7 @@ You may study `testCaclulate1` as a sample. This test case should make a connect
 
 What benefit do we have from having integration tests in our project?
 ```
-Your though here
+Ensuring that different components of your JSP application work together correctly when deployed on the Jetty server.
 ```
 
 
--- src/main/java/th/mfu/BMICalculatorServlet.java
+++ src/main/java/th/mfu/BMICalculatorServlet.java
@@ -8,21 +8,47 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-//TODO: add webservlet to "/calbmi"
-public class BMICalculatorServlet extends HttpServlet{
+@WebServlet("/calbmi")
+public class BMICalculatorServlet extends HttpServlet {
 
     @Override
     protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        //TODO: get parameter from request: "weight" and "height"
-        
-        //TODO: calculate bmi
+        // Get parameters from the request: "weight" and "height"
+        double weight = Double.parseDouble(request.getParameter("weight"));
+        double height = Double.parseDouble(request.getParameter("height"));
 
-        //TODO: determine the built from BMI
-      
-        //TODO: add bmi and built to the request's attribute
+        // Calculate BMI
+        double bmi = calculateBMI(weight, height);
 
-        //TODO: forward to jsp
-           
+        // Determine the built from BMI
+        String builtType = determineBuiltType(bmi);
+
+        // Round the BMI to an integer
+        int roundedBMI = (int) Math.round(bmi);
+
+        // Add BMI and built type to the request's attributes
+        request.setAttribute("bmi", roundedBMI);
+        request.setAttribute("builtType", builtType);
+
+        // Forward to jsp
+        request.getRequestDispatcher("/bmi_result.jsp").forward(request, response);
     }
-    
+
+    private double calculateBMI(double weight, double height) {
+        // BMI formula: weight (kg) / (height (m) * height (m))
+        return weight / (height * height);
+    }
+        private String determineBuiltType(double bmi) {
+        if (bmi < 18.5) {
+            return "underweight";
+        } else if (bmi < 25) {
+            return "normal";
+        } else if (bmi < 30) {
+            return "overweight";
+        } else if (bmi < 35) {
+            return "obese";
+        } else {
+            return "extremely obese";
+        }
+    }
 }
